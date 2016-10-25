b794df31b273542005748bbb94bda25e55a48eb1
Silvio T <stisa4@gmail.com>
2016-10-25 17:55:34 +0200

    hopefully fixed translation graph->matrix space

--- plot.nim
+++ plot.nim
@@ -33,32 +33,38 @@ const
   HalfTRed* = Color(0xFF000088)
   HalftWhite* = Color(0xFFFFFF88)
   
-proc proportionPixel(a:Axis,val:float):int =
-  if val-a.min.val < 0.00001: #precision
-    return a.min.pixel
-  let k = abs((a.max.val - val) / (val - a.min.val))
-  result = int( (a.max.pixel+a.min.pixel).float*k / (k+1) ) 
+proc pixelFromVal(a:Axis,val:float):int =
+  when defined debug:
+    echo "start pp"
+    echo "val",val
+    echo (val - a.min.val)
+    echo (a.max.val-a.min.val)
+    echo (a.max.pixel-a.min.pixel)
+    echo a.min.pixel
+    echo (((val - a.min.val)/(a.max.val-a.min.val) * (a.max.pixel-a.min.pixel).float)+(a.min.pixel).float)
+    echo "end pp"
+  result = (((val - a.min.val)/(a.max.val-a.min.val) * (a.max.pixel-a.min.pixel).float)+(a.min.pixel).float).int 
 
-proc `[]`(sur:Surface, x,y:int):Color =
+proc `[]`(sur:Surface, i,j:int):Color =
   ## x: position along the horizontal axis
   ## y: position along the vertical axis
   ## IN PIXEL
-  result = sur.pixels[(sur.height-1-y)*sur.width+x]
+  result = sur.pixels[i*(sur.width)+j]
 
-proc `[]=`(sur: var Surface, x,y:int, color:Color) =  
-
-  sur.pixels[(sur.height-1-y)*sur.width+x] = color
+proc `[]=`(sur: var Surface, i,j:int, color:Color) =  
+  echo i,j," ",sur.width
+  echo "comp",i*(sur.width)+j
+  sur.pixels[i*(sur.width)+j] = color
 
 proc `[]`(sur:Surface, x,y:float):Color =
   ## x: position along the horizontal axis
   ## y: position along the vertical axis
-  ## NOT IN PIXEL
-  echo sur.x.proportionPixel(x)," ",sur.y.proportionPixel(y)
-  result = sur[sur.x.proportionPixel(x),sur.y.proportionPixel(y)]
+  ## VALUES NOT IN PIXEL
+  result = sur[sur.y.pixelFromVal(y),sur.x.pixelFromVal(x)]
 
 proc `[]=`(sur: var Surface, x,y:float, color:Color) =
-  echo sur.x.proportionPixel(x)," ",sur.y.proportionPixel(y)  
-  sur[sur.x.proportionPixel(x),sur.y.proportionPixel(y)] = color
+  echo sur.y.pixelFromVal(y),sur.x.pixelFromVal(x)
+  sur[sur.y.pixelFromVal(y),sur.x.pixelFromVal(x)] = color
 
 proc fillWith*(sur: var Surface,color:Color=White) =
   ## Loop over every pixel in `img` and sets its color to `color` 
@@ -68,26 +74,13 @@ proc initAxis(v0,v1:float,p0,p1:int): Axis =
   result.max = (v1,p1)
   result.min = (v0,p0)
 
-
-proc initSurface(x0,x1,y0,y1:float) : Surface =
-  result.x = initAxis(x0,x1,0,1)
-  result.y = initAxis(y0,y1,0,1)
-  result.width = int(ceil(abs(x1-x0))) #wtf
-  result.height = int(ceil(abs(y1-y0)))
-  result.x.max.pixel = result.width
-  result.y.max.pixel = result.height
-  result.pixels = newSeq[Color](result.height*result.width)
-
 proc initSurface(x,y:Axis) : Surface =
   result.x = x
   result.y = y
-  result.width = int(ceil(abs(x.max.val-x.min.val))) #wtf
-  result.height = int(ceil(abs(y.max.val-y.min.val)))
-  result.x.max.pixel = result.width
-  result.y.max.pixel = result.height
-  result.pixels = newSeq[Color](result.height*result.width)
-  
-
+  result.width = abs(x.max.pixel-x.min.pixel)+1 #wtf
+  result.height = abs(y.max.pixel-y.min.pixel)+1
+  result.pixels = newSeq[Color](result.height*result.width)  
+  echo result.pixels.len
 proc saveSurfaceTo*(sur:Surface,filename:string) =
   ## Convience function. Saves `img` into `filename`
   var tmp = npng.initPNG(sur.width,sur.height,sur.pixels)
@@ -95,11 +88,26 @@ proc saveSurfaceTo*(sur:Surface,filename:string) =
 
 
 when isMainModule:
-  var rt = initSurface( -10.0,10.0, 0.0,20.0 )
+  var x,y : Axis
 
+  x = initAxis(0.0,10.0,0,9)
+  y = initAxis(0.0,10.0,9,0)
+  var rt = initSurface( x,y )
+  echo "x",x
+  echo "y",y
+  echo "0.0 on x: ",x.pixelFromVal(0.0)
+  echo "0.0 on y: ",y.pixelFromVal(0.0)
+
+  echo "5.0 on x: ",x.pixelFromVal(5.0)
+  echo "5.0 on y: ",y.pixelFromVal(5.0)
   rt.fillWith(Yellow)
-  for i in 0..10:
-    rt[float(i),i.float*2] = Black
+  rt[0.0,0.0] = Black
+  rt[5.0,5.0] = Red
+  
+  
+  for i in 0..<rt.width:
+  #  echo "f",float(i)
+    rt[float(i),i.float*0.5] = Black
   ## Plot x,y with color `lncolor` and `scale`
   # TODO: have a switch to use antialiased lines
   #rt.drawLine(0,0,5,5,Red)
