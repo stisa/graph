59f1a18b15c7ae2ece1efe79aac8b672f47b8401
Silvio T <stisa4@gmail.com>
2016-10-25 18:18:54 +0200

    reimplement line drawing

--- plot.nim
+++ plot.nim
@@ -52,8 +52,6 @@ proc `[]`(sur:Surface, i,j:int):Color =
   result = sur.pixels[i*(sur.width)+j]
 
 proc `[]=`(sur: var Surface, i,j:int, color:Color) =  
-  echo i,j," ",sur.width
-  echo "comp",i*(sur.width)+j
   sur.pixels[i*(sur.width)+j] = color
 
 proc `[]`(sur:Surface, x,y:float):Color =
@@ -80,34 +78,65 @@ proc initSurface(x,y:Axis) : Surface =
   result.width = abs(x.max.pixel-x.min.pixel)+1 #wtf
   result.height = abs(y.max.pixel-y.min.pixel)+1
   result.pixels = newSeq[Color](result.height*result.width)  
-  echo result.pixels.len
+  #echo result.pixels.len
+
 proc saveSurfaceTo*(sur:Surface,filename:string) =
   ## Convience function. Saves `img` into `filename`
   var tmp = npng.initPNG(sur.width,sur.height,sur.pixels)
   tmp.writeToFile(filename)
 
+proc drawLine*(srf:var Surface, x1,y1,x2,y2:int, color : Color = Black) =
+  ## Draws a line between x1,y1 and x2,y2. Uses Bresenham's line algorithm.
+  var dx = x2-x1
+  var dy = y2-y1
+  
+  let ix = if dx > 0 : 1 elif dx<0 : -1 else: 0
+  let iy = if dy > 0 : 1 elif dy<0 : -1 else: 0
+
+  dx = abs(dx) shl 1
+  dy = abs(dy) shl 1
+
+  var xi = x1
+  var yi = y1
+
+  srf[yi,xi] = color
+  
+  if dx>=dy:
+    var err = dy-(dx shr 1)
+    while xi != x2:
+      if (err >= 0):
+        err -= dx
+        yi+=iy
+      
+      err += dy
+      xi += ix
+      srf[yi,xi] = color
+  else:
+    var err = dx - (dy shr 1)
+    while yi!=y2:
+      if err >= 0:
+        err -= dy
+        xi += ix
+      err += dx
+      yi += iy
+      srf[yi,xi] = color
+
+proc drawLine*(srf:var Surface, x1,y1,x2,y2:float, color : Color = Black) {.inline.} =
+  srf.drawLine(srf.x.pixelFromVal(x1),srf.y.pixelFromVal(y1),srf.x.pixelFromVal(x2),srf.y.pixelFromVal(y2), color)
 
 when isMainModule:
   var x,y : Axis
 
-  x = initAxis(0.0,10.0,0,9)
-  y = initAxis(0.0,10.0,9,0)
+  x = initAxis(0.0,10.0,0,99)
+  y = initAxis(0.0,10.0,99,0)
   var rt = initSurface( x,y )
-  echo "x",x
-  echo "y",y
-  echo "0.0 on x: ",x.pixelFromVal(0.0)
-  echo "0.0 on y: ",y.pixelFromVal(0.0)
 
-  echo "5.0 on x: ",x.pixelFromVal(5.0)
-  echo "5.0 on y: ",y.pixelFromVal(5.0)
   rt.fillWith(Yellow)
   rt[0.0,0.0] = Black
   rt[5.0,5.0] = Red
   
+  rt.drawLine(0.0,0.0,5.0,5.0)
   
-  for i in 0..<rt.width:
-  #  echo "f",float(i)
-    rt[float(i),i.float*0.5] = Black
   ## Plot x,y with color `lncolor` and `scale`
   # TODO: have a switch to use antialiased lines
   #rt.drawLine(0,0,5,5,Red)
