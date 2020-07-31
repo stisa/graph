0981d7d830d04f2fd822a1cf121674330d568b8e
stisa <silvio@tisato.me>
2020-07-31 02:03:31 +0200

    add jupyterPlotData helper

--- src/graph.nim
+++ src/graph.nim
@@ -1,5 +1,5 @@
 import nimPNG
-import math
+import math, base64, streams
 import graph/plot
 import graph/draw
 import graph/funcs
@@ -12,6 +12,24 @@ proc saveTo*(sur:Surface,filename:string) =
   var px = ""
   for p in sur.pixels: px.add($p)
   if not savepng32(filename,px,sur.width,sur.height): assert(false,"Error saving")
+
+proc jupyterPlotData*(sur:Surface): string =
+  ## Returns the plot in the base64-"#>jnps0000x0000"/"jnps<#" delimited
+  ## format jupyternim expects
+  var px = ""
+  for p in sur.pixels: px.add($p)
+  var ss = newStringStream("")
+  writeChunks(encodePNG32(px,sur.width,sur.height), ss)
+  ss.setPosition(0)
+
+  var sw = $sur.width
+  var sh = $sur.height
+  #FIXME: assumes w/h are either 3 or 4 digits
+  if sw.len!=4: sw = "0" & sw
+  if sh.len!=4: sh = "0" & sh
+
+  result = "#>jnps" & sw & "x" & sh & ss.readAll.encode() & "jnps<#"
+
 
 proc flipX*(srf:var Surface) =
 # TODO: swap
@@ -53,10 +71,11 @@ proc drawProc*[T](sur:var Surface, x:openarray[T], fn: proc(o:T):T, lncolor:Colo
 
 when isMainModule:
   import graph/color,graph/draw
-  var rt = initSurface( 0,10,0,10 )
+  var rt = initSurface( 0,160,0,120 )
 
   rt.fillWith(Yellow)
   ## Plot x,y with color `lncolor` and `scale`
   # TODO: have a switch to use antialiased lines
   #rt.line(0,0,5,5,Red)
   rt.saveTo("test.png")
+  echo rt.jupyterPlotData
